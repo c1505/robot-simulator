9e4676cab0537eac981c76bf904e50e34aa8058f
c1505 <coreym247@gmail.com>
2017-07-13 18:16:18 +0000

    robot turns left and right

--- robot_simulator.rb
+++ robot_simulator.rb
@@ -1,9 +1,25 @@
+require 'pry'
 class Robot
+  ORIENTATIONS = [:north, :east, :south, :west]
   def orient(direction)
+    raise ArgumentError unless ORIENTATIONS.include?(direction) 
     @bearing = direction
   end
   
   def bearing
     @bearing
+  end
+  
+  def turn_right
+    next_index = ORIENTATIONS.index(@bearing) + 1
+    if next_index == 4
+      next_index = 0
+    end
+    @bearing = ORIENTATIONS[next_index]
+  end
+  
+  def turn_left
+    next_index = ORIENTATIONS.index(@bearing) - 1
+    @bearing = ORIENTATIONS[next_index]
   end
 end
